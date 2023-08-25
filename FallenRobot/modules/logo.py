e8e842d548305d66c0201abd0d6899eedec6792d
Rᴀᴅʜᴀ Sɪɴɢʜ <133753917+Radhak8@users.noreply.github.com>
2023-08-25 14:19:56 +0530

    Update logo.py

--- FallenRobot/modules/logo.py
+++ FallenRobot/modules/logo.py
@@ -294,7 +294,7 @@ async def lego(event):
         if os.path.exists(fname):
             os.remove(fname)
     except Exception as e:
-        await event.reply(f"ғʟᴏᴏᴅᴡᴀɪᴛ ᴇʀʀᴏʀ, ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @RadhaX2Support")
+        await event.reply(f"ғʟᴏᴏᴅᴡᴀɪᴛ ᴇʀʀᴏʀ, ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @RadhaSupport")
 
 
 __mod_name__ = "Lᴏɢᴏ​"
